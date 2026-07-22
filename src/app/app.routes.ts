ebe6e4e524ef0dcf131e394585497ea96115fc3d
rcaa <kalisabelyseteta@gmail.com>
2026-07-22 10:03:51 +0200

    define lazy-loaded app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./home/home').then((m) => m.Home),
+  },
+  {
+    path: 'counter',
+    loadComponent: () => import('./counter/counter').then((m) => m.Counter),
+  },
+];
