6eac50fcdac795a68153f5b6f85a6dff3b57ff33
rcaa <kalisabelyseteta@gmail.com>
2026-07-24 16:41:59 +0200

    build the feedback form with signal forms

--- src/app/feedback/feedback.ts
+++ src/app/feedback/feedback.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
+
+interface FeedbackFormValue {
+  name: string;
+  email: string;
+  message: string;
+}
 
 @Component({
   selector: 'app-feedback',
-  imports: [],
+  imports: [FormField],
   templateUrl: './feedback.html',
   styleUrl: './feedback.scss',
 })
-export class Feedback {}
+export class Feedback {
+  private readonly model = signal<FeedbackFormValue>({ name: '', email: '', message: '' });
+
+  protected readonly feedbackForm = form(this.model);
+}
