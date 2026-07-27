3f6aea527d2f5ce334203d98e31d5718fa25115d
rcaa <kalisabelyseteta@gmail.com>
2026-07-27 08:46:44 +0200

    list favorited pages on Home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Favorites } from '../favorites';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  protected readonly favorites = inject(Favorites);
+}
