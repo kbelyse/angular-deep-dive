39abb6524d6c2144fda594d36a61e176a78b9f54
rcaa <kalisabelyseteta@gmail.com>
2026-07-28 11:26:44 +0200

    defer-load RecentPosts on Home with @defer (on viewport)

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Favorites } from '../favorites';
+import { RecentPosts } from './recent-posts/recent-posts';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, RecentPosts],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
