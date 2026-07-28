2695f346261b24fc8ce7eb7fdea35f331f0197dd
rcaa <kalisabelyseteta@gmail.com>
2026-07-28 11:02:33 +0200

    fetch posts with httpResource

--- src/app/posts/posts.spec.ts
+++ src/app/posts/posts.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { Posts } from './posts';
 
@@ -9,6 +11,7 @@ describe('Posts', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Posts],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Posts);
--- src/app/posts/posts.ts
+++ src/app/posts/posts.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { httpResource } from '@angular/common/http';
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts?_limit=10';
 
 @Component({
   selector: 'app-posts',
@@ -6,4 +15,6 @@ import { Component } from '@angular/core';
   templateUrl: './posts.html',
   styleUrl: './posts.scss',
 })
-export class Posts {}
+export class Posts {
+  protected readonly postsResource = httpResource<Post[]>(() => POSTS_URL, { defaultValue: [] });
+}
