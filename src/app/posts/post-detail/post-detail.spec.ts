8a3a058c61e0c0a5c6b3e4a17df31f7fb5ef9b9d
rcaa <kalisabelyseteta@gmail.com>
2026-07-29 15:33:30 +0200

    add a back-to-posts link on PostDetail

--- src/app/posts/post-detail/post-detail.spec.ts
+++ src/app/posts/post-detail/post-detail.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
 
 import { PostDetail } from './post-detail';
 
@@ -12,7 +13,7 @@ describe('PostDetail', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PostDetail],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [provideHttpClient(), provideHttpClientTesting(), provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PostDetail);
