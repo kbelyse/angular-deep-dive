4397a5df1e492ff50118fb15ebcb144e7eec1307
rcaa <kalisabelyseteta@gmail.com>
2026-07-30 11:22:35 +0200

    add Tab component: content-projected panel with activate/deactivate

---
+++ src/app/tabs/tab.ts
@@ -0,0 +1,32 @@
+import { Component, computed, input, signal } from '@angular/core';
+
+let nextTabId = 0;
+
+@Component({
+  selector: 'app-tab',
+  template: `
+    @if (active()) {
+      <div role="tabpanel" [id]="panelId()" [attr.aria-labelledby]="tabId()" tabindex="0">
+        <ng-content />
+      </div>
+    }
+  `,
+})
+export class Tab {
+  readonly title = input.required<string>();
+
+  private readonly uid = nextTabId++;
+  readonly panelId = computed(() => `tabpanel-${this.uid}`);
+  readonly tabId = computed(() => `tab-${this.uid}`);
+
+  private readonly activeState = signal(false);
+  readonly active = this.activeState.asReadonly();
+
+  activate(): void {
+    this.activeState.set(true);
+  }
+
+  deactivate(): void {
+    this.activeState.set(false);
+  }
+}
