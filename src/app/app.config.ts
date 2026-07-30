89db755e859fa4d3cab9c476bf49ede0bb0b3ec2
rcaa <kalisabelyseteta@gmail.com>
2026-07-30 11:20:19 +0200

    wire loadingInterceptor into the app's HttpClient provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,16 +3,17 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { loadingInterceptor } from './loading-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([loadingInterceptor])),
   ],
 };
