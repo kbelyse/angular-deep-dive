20701cdc335bedb6186b347cf86c67b2c691db65
rcaa <kalisabelyseteta@gmail.com>
2026-07-30 11:25:04 +0200

    import Tabs/Tab in Home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Favorites } from '../favorites';
+import { Tab } from '../tabs/tab';
+import { Tabs } from '../tabs/tabs';
 import { RecentPosts } from './recent-posts/recent-posts';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, RecentPosts],
+  imports: [RouterLink, RecentPosts, Tabs, Tab],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
