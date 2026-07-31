1cef40f34c81cd293c4e82e5ba13280dc2c718e7
rcaa <kalisabelyseteta@gmail.com>
2026-07-31 10:57:28 +0200

    wire SelectivePreloadingStrategy into the router config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,16 +4,17 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { loadingInterceptor } from './loading-interceptor';
+import { SelectivePreloadingStrategy } from './selective-preloading-strategy';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), withPreloading(SelectivePreloadingStrategy)),
     provideHttpClient(withInterceptors([loadingInterceptor])),
   ],
 };
