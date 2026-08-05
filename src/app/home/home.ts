8900e52c3f6f330c693e46868e452b19c818b6bf
kbelyse <kalisabelyseteta@gmail.com>
2026-08-05 11:23:31 +0200

    add a Clear all favorites action to Home, guarded by ConfirmDialog

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ConfirmDialog } from '../confirm-dialog/confirm-dialog';
 import { Favorites } from '../favorites';
 import { Tab } from '../tabs/tab';
 import { Tabs } from '../tabs/tabs';
@@ -7,10 +8,25 @@ import { RecentPosts } from './recent-posts/recent-posts';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, RecentPosts, Tabs, Tab],
+  imports: [RouterLink, RecentPosts, Tabs, Tab, ConfirmDialog],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   protected readonly favorites = inject(Favorites);
+
+  protected readonly confirmingClear = signal(false);
+
+  protected requestClear(): void {
+    this.confirmingClear.set(true);
+  }
+
+  protected confirmClear(): void {
+    this.favorites.clear();
+    this.confirmingClear.set(false);
+  }
+
+  protected cancelClear(): void {
+    this.confirmingClear.set(false);
+  }
 }
